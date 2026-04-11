6e2f71eccde9f55be9c5667f98a5a093fbe0be01
coucoumonecho <coucoumonecho@gmail.com>
2026-04-11 14:40:09 +0800

    recall metric

--- app/agent/state.py
+++ app/agent/state.py
@@ -1,6 +1,42 @@
 from typing import TypedDict
 
 from app.entities.column_info import ColumnInfo
+from app.entities.metric_info import MetricInfo
+from app.entities.value_info import ValueInfo
+
+
+class MetricInfoState(TypedDict):
+    name: str
+    description: str
+    relevant_columns: list[str]
+    alias: list[str]
+
+
+class ColumnInfoState(TypedDict):
+    name: str
+    type: str
+    role: str
+    examples: list
+    description: str
+    alias: list[str]
+
+
+class TableInfoState(TypedDict):
+    name: str
+    role: str
+    description: str
+    columns: list[ColumnInfoState]
+
+
+class DateInfoState(TypedDict):
+    date: str
+    weekday: str
+    quarter: str
+
+
+class DBInfoState(TypedDict):
+    dialect: str
+    version: str
 
 
 class DataAgentState(TypedDict):
@@ -8,5 +44,15 @@ class DataAgentState(TypedDict):
     keywords: list[str]  # 抽取的关键词
 
     retrieved_column_infos: list[ColumnInfo]  # 检索到的字段信息
+    retrieved_metric_infos: list[MetricInfo]  # 检索到的指标信息
+    retrieved_value_infos: list[ValueInfo]  # 检索到的取值信息
+
+    table_infos: list[TableInfoState]  # 表信息
+    metric_infos: list[MetricInfoState]  # 指标信息
+
+    date_info: DateInfoState  # 日期信息
+    db_info: DBInfoState  # 数据库信息
+
+    sql: str  # 生成的SQL
 
     error: str  # 校验SQL时出现的错误信息
